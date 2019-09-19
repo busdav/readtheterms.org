da333a35f74fe1b40f5f49e4826468086c0db30e
David Buser <busdav@outlook.com>
2019-09-19 23:47:55 +0100

    Abstract app.js into app-config, route-config, db-config

--- app.js
+++ app.js
@@ -1,52 +1,12 @@
-require('dotenv').config();
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-
-const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+const appConfig = require('./src/config/app-config.js');
+const routeConfig = require('./src/config/route-config.js');
+const dbConfig = require('./src/config/db-config.js');
 
 const app = express();
 
-// Set up default mongoose connection
-const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_PATH}`;
-mongoose.connect(mongoUri, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-  console.log(`Successfully connected to ${mongoUri}`);
-});
-
-// view engine setup
-app.set('views', path.join(__dirname, 'src', 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+appConfig.init(app, express);
+routeConfig.init(app);
+dbConfig.init();
 
 module.exports = app;
---
+++ src/config/db-config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+module.exports = {
+  init() {
+    // Set up default mongoose connection
+    const mongoUri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_PATH}`;
+    mongoose.connect(mongoUri, { useNewUrlParser: true });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', function() {
+      console.log(`Successfully connected to ${mongoUri}`);
+    });
+  },
+};
