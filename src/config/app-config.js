4ec86bb00835ef0c2e984f55556026da2d7c77cd
David Buser <busdav@outlook.com>
2019-09-20 17:04:12 +0100

    Eslint

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('path'); 
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -10,7 +10,9 @@ module.exports = {
 
     app.use(logger('dev'));
     app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+    app.use(express.urlencoded({
+      extended: false
+    }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, '..', '..', 'public')));
   },
