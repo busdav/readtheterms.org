0df493b633181f774aa43b9865d20061d8709cc4
David Buser <busdav@outlook.com>
2019-09-20 19:49:30 +0100

    Try Eslint in backend

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,11 +2,11 @@ const createError = require("http-errors");
 
 module.exports = {
   init(app) {
-    const indexRouter = require('../routes/index');
-    const usersRouter = require('../routes/users');
+    const indexRouter = require("../routes/index");
+    const usersRouter = require("../routes/users");
 
-    app.use('/', indexRouter);
-    app.use('/users', usersRouter);
+    app.use("/", indexRouter);
+    app.use("/users", usersRouter);
 
     // catch 404 and forward to error handler
     app.use(function(req, res, next) {
@@ -17,11 +17,11 @@ module.exports = {
     app.use(function(err, req, res, next) {
       // set locals, only providing error in development
       res.locals.message = err.message;
-      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      res.locals.error = req.app.get("env") === "development" ? err : {};
 
       // render the error page
       res.status(err.status || 500);
-      res.render('error');
+      res.render("error");
     });
-  },
+  }
 };
