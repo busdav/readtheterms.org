d1eaaa08d6c69d2063af28004bc832ba91db4986
David Buser <busdav@outlook.com>
2019-11-07 11:37:29 +0000

    test

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-let test;
-
 // GET blog page
 router.get("/blog", function(req, res) {
   res.send("Welcome to the blog!");
