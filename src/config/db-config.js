c333b3c4d57904e434e708350ebc70ad3511c927
David Buser <busdav@outlook.com>
2019-11-11 09:19:11 +0000

    Finish implementing Jest

--- src/config/db-config.js
+++ src/config/db-config.js
@@ -4,15 +4,28 @@ const mongoose = require("mongoose");
 module.exports = {
   init() {
     // Set up default mongoose connection
-    const mongoUri = process.env.MONGODB_URI;
-    mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    const db = mongoose.connection;
-    db.on("error", console.error.bind(console, "MongoDB connection error:"));
-    db.once("open", function() {
-      console.log(`Successfully connected to ${mongoUri}`);
-    });
+    if (process.env.NODE_ENV === "test") {
+      const mongoUri = process.env.MONGODB_URI_TEST;
+      mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      const db = mongoose.connection;
+      db.on("error", console.error.bind(console, "MongoDB connection error:"));
+      db.once("open", function() {
+        console.log(`Successfully connected to ${mongoUri}`);
+      });
+    } else {
+      const mongoUri = process.env.MONGODB_URI_DEV;
+      mongoose.connect(mongoUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      const db = mongoose.connection;
+      db.on("error", console.error.bind(console, "MongoDB connection error:"));
+      db.once("open", function() {
+        console.log(`Successfully connected to ${mongoUri}`);
+      });
+    }
   }
 };
